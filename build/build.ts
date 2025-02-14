abec11acb124906673a86ebf75030e98cc915c40
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-02-14 19:02:36 +0000

    add hyperliquid

--- build/build.ts
+++ build/build.ts
@@ -6,13 +6,77 @@ const ASYNC_INIT = "./hyperliquid/async_support/__init__.py";
 
 
 
-async function cleanInit() {
-    const fileContent = fs.readFileSync(SYNC_INIT, 'utf8');
-    const fileByLine = fileContent.split('\n');
+function regexAll (text, array) {
+    for (const i in array) {
+        let regex = array[i][0]
+        const flags = (typeof regex === 'string') ? 'g' : undefined
+        regex = new RegExp (regex, flags)
+        text = text.replace (regex, array[i][1])
+    }
+    return text
+}
 
-    for 
+
+
+async function cleanInit() {
+    let fileContent = fs.readFileSync(SYNC_INIT, 'utf8');
+    // const fileByLine = fileContent.split('\n');
+
+    fileContent = regexAll (fileContent, [
+        // [ /from ccxt\./gm, '' ], // new esm
+    ]).trim ()
+
+    const file: string[] = []
+    const fileLines = fileContent.split('\n');
+
+
+    const pattern = /^from ccxt\.([a-zA-Z0-9_]+) import \1.+/;
+    let insideExchange = false
+    for (const line of fileLines) {
+        if (new RegExp(pattern).test(line)) {
+            continue;
+        }
+
+        if (line.startsWith('#') || line.trim() === '') {
+            file.push(line);
+            continue;
+        }
+
+        if (line === 'exchanges = [') {
+            insideExchange = true;
+            continue;
+        }
+
+        if (insideExchange && line === ']') {
+            insideExchange = false;
+            continue;
+        }
+
+        if (insideExchange) {
+            continue
+        }
+
+        if (line.startsWith('__all__')) {
+            continue;
+        }
+
+        file.push(line);
+    }
+
+    file.push("")
+    file.push("import hyperliquid")
+
+    let newFileContent = file.join('\n');
+    newFileContent = regexAll (newFileContent, [
+        [ /from ccxt\./gm, 'from ' ],
+    ]).trim ()
+
+    // save file
+    fs.writeFileSync(SYNC_INIT, newFileContent);
 }
 
 async function main() {
     await cleanInit();
 }
+
+main()
---
+++ hyperliquid/__init__.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+"""CCXT: CryptoCurrency eXchange Trading Library"""
+
+# MIT License
+# Copyright (c) 2017 Igor Kroitor
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# ----------------------------------------------------------------------------
+
+__version__ = '4.4.59'
+
+# ----------------------------------------------------------------------------
+
+from base.exchange import Exchange                     # noqa: F401
+from base.precise import Precise                       # noqa: F401
+
+from base.decimal_to_precision import decimal_to_precision  # noqa: F401
+from base.decimal_to_precision import TRUNCATE              # noqa: F401
+from base.decimal_to_precision import ROUND                 # noqa: F401
+from base.decimal_to_precision import ROUND_UP              # noqa: F401
+from base.decimal_to_precision import ROUND_DOWN            # noqa: F401
+from base.decimal_to_precision import DECIMAL_PLACES        # noqa: F401
+from base.decimal_to_precision import SIGNIFICANT_DIGITS    # noqa: F401
+from base.decimal_to_precision import TICK_SIZE             # noqa: F401
+from base.decimal_to_precision import NO_PADDING            # noqa: F401
+from base.decimal_to_precision import PAD_WITH_ZERO         # noqa: F401
+
+from base import errors
+from base.errors import BaseError                                # noqa: F401
+from base.errors import ExchangeError                            # noqa: F401
+from base.errors import AuthenticationError                      # noqa: F401
+from base.errors import PermissionDenied                         # noqa: F401
+from base.errors import AccountNotEnabled                        # noqa: F401
+from base.errors import AccountSuspended                         # noqa: F401
+from base.errors import ArgumentsRequired                        # noqa: F401
+from base.errors import BadRequest                               # noqa: F401
+from base.errors import BadSymbol                                # noqa: F401
+from base.errors import OperationRejected                        # noqa: F401
+from base.errors import NoChange                                 # noqa: F401
+from base.errors import MarginModeAlreadySet                     # noqa: F401
+from base.errors import MarketClosed                             # noqa: F401
+from base.errors import ManualInteractionNeeded                  # noqa: F401
+from base.errors import InsufficientFunds                        # noqa: F401
+from base.errors import InvalidAddress                           # noqa: F401
+from base.errors import AddressPending                           # noqa: F401
+from base.errors import InvalidOrder                             # noqa: F401
+from base.errors import OrderNotFound                            # noqa: F401
+from base.errors import OrderNotCached                           # noqa: F401
+from base.errors import OrderImmediatelyFillable                 # noqa: F401
+from base.errors import OrderNotFillable                         # noqa: F401
+from base.errors import DuplicateOrderId                         # noqa: F401
+from base.errors import ContractUnavailable                      # noqa: F401
+from base.errors import NotSupported                             # noqa: F401
+from base.errors import InvalidProxySettings                     # noqa: F401
+from base.errors import ExchangeClosedByUser                     # noqa: F401
+from base.errors import OperationFailed                          # noqa: F401
+from base.errors import NetworkError                             # noqa: F401
+from base.errors import DDoSProtection                           # noqa: F401
+from base.errors import RateLimitExceeded                        # noqa: F401
+from base.errors import ExchangeNotAvailable                     # noqa: F401
+from base.errors import OnMaintenance                            # noqa: F401
+from base.errors import InvalidNonce                             # noqa: F401
+from base.errors import ChecksumError                            # noqa: F401
+from base.errors import RequestTimeout                           # noqa: F401
+from base.errors import BadResponse                              # noqa: F401
+from base.errors import NullResponse                             # noqa: F401
+from base.errors import CancelPending                            # noqa: F401
+from base.errors import UnsubscribeError                         # noqa: F401
+from base.errors import error_hierarchy                          # noqa: F401
+
+
+
+base = [
+    'Exchange',
+    'Precise',
+    'exchanges',
+    'decimal_to_precision',
+]
+
+import hyperliquid
