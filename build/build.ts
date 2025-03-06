076b67a613003f6819969547370f3a9fa2182e72
t.t <7117978+ttodua@users.noreply.github.com>
2025-03-07 00:39:42 +0400

    move to last line

--- build/build.ts
+++ build/build.ts
@@ -121,13 +121,14 @@ class build {
 
     async init (exchange:string) {
         await this.downloadRepo();
-        await this.setAllExchangesList();
         this.moveFiles(exchange);
-        // Remove git dir now (after reading exchanges)
-        fs.rmSync(__dirname + '/ccxt/', { recursive: true, force: true });
+        await this.setAllExchangesList();
 
         await this.cleanInitFile(this.destinationFolder + '__init__.py');
         await this.cleanInitFile(this.destinationFolder + 'async_support/__init__.py', true);
+
+        // Remove git dir now (after reading exchanges)
+        fs.rmSync(__dirname + '/ccxt/', { recursive: true, force: true });
     }
 }
 
